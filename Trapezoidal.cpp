01f7f8d4c0344ee8801fff0b91da0331aef459af
Omar Lawatey <omarlawatey@gmail.com>
2023-12-07 01:28:28 +0200

    Feat: Add custom intervals & added executable file

--- Trapezoidal.cpp
+++ Trapezoidal.cpp
@@ -8,12 +8,29 @@ float function(float x)
   return sqrt(1 + pow(x, 3));
 }
 
+float function_compiler(string human_written_function)
+{
+  return 0;
+}
+
 int main()
 {
   string func = "";
 
-  int n = 999999;
+  int n = 0;
   float a, b, h, sum = 0, I = 0, err;
+
+  cout << "Enter the interval: ";
+  cin >> n;
+
+  if (n > 999999 || n < 1)
+  {
+    do
+    {
+      cout << "You can't enter a number bigger than 999999. The interval can't be less than 1.\nEnter the interval: ";
+      cin >> n;
+    } while (n > 999999 || n < 1);
+  }
 
   cout << "Enter the lower limit: ";
   cin >> a;
@@ -23,14 +40,16 @@ int main()
 
   h = abs((a - b) / n);
 
-  for (int i = 1; i <= n; i++)
+  for (int i = 1; i < n; i++)
   {
     sum += function(a + i * h);
   }
 
   I = (function(a) + (sum * 2) + function(b)) * (h / 2);
 
-  cout << "The value of I: " << I;
+  cout << "The value of I: " << I << endl;
+
+  system("pause");
 
   return 0;
 }
