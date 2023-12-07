72696b14565dc10ce2f2bc1014b39f1d9b43d70a
Omar Lawatey <omarlawatey@gmail.com>
2023-12-08 00:44:56 +0200

    Refact: Restructure the code

--- Trapezoidal.cpp
+++ Trapezoidal.cpp
@@ -3,40 +3,15 @@
 
 using namespace std;
 
-float function(float x)
+double function(double x)
 {
   return sqrt(1 + pow(x, 3));
 }
 
-float function_compiler(string human_written_function)
+double get_integration(int n, float a, float b)
 {
-  return 0;
-}
-
-int main()
-{
-  string func = "";
-
-  long long n = 1, max_n = 999999;
-  float a = 0, b = 0, h = 0, I = 0, sum = 0, err;
-
-  do
-  {
-    if (n <= 0)
-      cout << "The interval can't be less than 1." << endl;
-
-    if (n > 999999)
-      cout << "You can't exced 9223372036854775807." << endl;
-
-    cout << "Enter the interval: ";
-    cin >> n;
-  } while (n > 999999 || n <= 0);
-
-  cout << "Enter the lower limit: ";
-  cin >> a;
-
-  cout << "Enter the upper limit: ";
-  cin >> b;
+  float h = 0;
+  double sum = 0, I = 0;
 
   h = abs((a - b) / n);
 
@@ -47,9 +22,40 @@ int main()
 
   I = (function(a) + (sum * 2) + function(b)) * (h / 2);
 
-  cout << "The value of I: " << I << endl;
+  return I;
+}
+
+int main()
+{
+  int n = 1, max_n = 999999999;
+  float a = 0, b = 0;
+  double I = 0, nearest_I = 0, err = 0;
+
+  do
+  {
+    if (n <= 0)
+      cout << "The interval can't be less than 1." << endl;
+
+    if (n > max_n)
+      cout << "You can't exced " << max_n << "." << endl;
+
+    cout << "Enter the interval: ";
+    cin >> n;
+  } while (n > max_n || n <= 0);
+
+  cout << "Enter the lower limit: ";
+  cin >> a;
+
+  cout << "Enter the upper limit: ";
+  cin >> b;
+
+  nearest_I = get_integration(max_n, a, b);
+  I = get_integration(n, a, b);
+
+  err = abs((I - nearest_I) / nearest_I) * 100;
+
+  cout << "The value of I is close to: " << I << "\nError percentage is nearly: " << err << "%" << endl;
 
   system("pause");
-
   return 0;
 }
