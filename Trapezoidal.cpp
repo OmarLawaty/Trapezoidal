8d7145862b0a6a6711e6ecb54d49114dfb884d9b
Omar Lawatey <omarlawatey@gmail.com>
2023-12-18 14:33:18 +0200

    Feat: Implement approximate error values
    Add Richardson extrapolation rule to estimate error percentage

--- Trapezoidal.cpp
+++ Trapezoidal.cpp
@@ -3,59 +3,79 @@
 
 using namespace std;
 
-double function(double x)
+// Defining the target function
+double target_function(double x)
 {
-  return sqrt(1 + pow(x, 3));
+  return sqrt(1 + pow(x, 3)); // Function: sqrt(1 + x^3)
 }
 
+// Trapezoidal rule for numerical integration
 double get_integration(int n, float a, float b)
 {
   float h = 0;
   double sum = 0, I = 0;
 
-  h = abs((a - b) / n);
+  h = abs((a - b) / n); // Calculate step size
 
+  // Calculate the integral using the trapezoidal rule
   for (int i = 1; i < n; i++)
   {
-    sum += function(a + i * h);
+    sum += target_function(a + i * h); // Accumulate function values
   }
 
-  I = (function(a) + (sum * 2) + function(b)) * (h / 2);
+  I = (target_function(a) + (sum * 2) + target_function(b)) * (h / 2); // Final integral estimation
 
-  return I;
+  return I; // Return the estimated integral value
+}
+
+// Richardson extrapolation to estimate error percentage
+double estimateError(int n, double a, double b)
+{
+  // Calculating integrals with different step sizes
+  double I_h = get_integration(n, a, b);      // Result with step size h
+  double I_h2 = get_integration(2 * n, a, b); // Result with step size h/2
+  double I_h4 = get_integration(4 * n, a, b); // Result with step size h/4
+
+  // Richardson extrapolation formula to estimate error
+  double error = (1.0 / 15.0) * (I_h4 - 16 * I_h2 + 15 * I_h);
+  return fabs(error) * 100; // Return absolute value of the error as a percentage
 }
 
 int main()
 {
-  int n = 1, max_n = 999999999;
+  int n = 1, max_n = 99999;
   float a = 0, b = 0;
-  double I = 0, nearest_I = 0, err = 0;
+  double I = 0, estimated_error = 0;
+  string user_written_function = "";
 
   do
   {
+    // Validate the interval input
     if (n <= 0)
       cout << "The interval can't be less than 1." << endl;
 
     if (n > max_n)
-      cout << "You can't exced " << max_n << "." << endl;
+      cout << "You can't exceed " << max_n << "." << endl;
 
     cout << "Enter the interval: ";
     cin >> n;
   } while (n > max_n || n <= 0);
 
+  // Input lower and upper limits for integration
   cout << "Enter the lower limit: ";
   cin >> a;
 
   cout << "Enter the upper limit: ";
   cin >> b;
 
-  nearest_I = get_integration(max_n, a, b);
+  // Calculate integral and estimated error
   I = get_integration(n, a, b);
+  estimated_error = estimateError(n, a, b);
 
-  err = abs((I - nearest_I) / nearest_I) * 100;
+  // Display results
+  cout << "Integral is approximately: " << I << endl;
+  cout << "Estimated error percentage: " << estimated_error << "%" << endl;
 
-  cout << "The value of I is close to: " << I << "\nError percentage is nearly: " << err << "%" << endl;
-
-  system("pause");
+  system("pause"); // Pause the console before exiting
   return 0;
 }
